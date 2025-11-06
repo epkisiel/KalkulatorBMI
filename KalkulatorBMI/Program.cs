f26cb115325b54c8077e22c96169c0090fde7c2c
4Mgr1 <4Mgr1@DESKTOP-EHD8S3P>
2025-11-06 09:54:03 +0100

    Dodaj pliki projektów.

---
+++ KalkulatorBMI/Program.cs
@@ -0,0 +1,43 @@
+﻿public class KalkulatorBMI
+{
+    public string BMI { get; set; }
+    public double ObliczBMI(double wzrostCm, double wagaKg)
+    {
+        return wagaKg / ((wzrostCm / 100) * (wzrostCm / 100));
+    }
+    public string InterpretujBMI(double bmi)
+    {
+        if (bmi < 18.5)
+            return "Niedowaga";
+        else if (bmi >= 18.5 && bmi <= 24.9)
+            return "Waga prawidłowa";
+        else if (bmi >= 25 && bmi <=29.9)
+            return "Nadwaga";
+        else
+            return "Otyłość";
+    }
+}
+
+
+Console.WriteLine("Podaj wzrost:");
+string podanyWzrost = Console.ReadLine();
+while (double.TryParse(podanyWzrost, out double w) == false || w <= 0)
+{
+    Console.WriteLine("Podano nieprawidłowy wzrost. Podaj jeszce raz:");
+    podanyWzrost = Console.ReadLine();
+}
+double.TryParse(podanyWzrost, out double wzrost);
+
+Console.WriteLine("Podaj wagę:");
+string podanaWaga = Console.ReadLine();
+while (double.TryParse(podanaWaga, out double w) == false || w <= 0)
+{
+    Console.WriteLine("Podano nieprawidłową wagę. Podaj jeszce raz:");
+    podanaWaga = Console.ReadLine();
+}
+double.TryParse(podanaWaga, out double waga);
+
+
+var kalkulator = new KalkulatorBMI();
+double obliczoneBMI = kalkulator.ObliczBMI(wzrost, waga);
+kalkulator.BMI = kalkulator.InterpretujBMI(obliczoneBMI);
